52758279a720efc426782b408735165c33053861
Rahul Rawat <rahulrawat593@gmail.com>
2015-05-08 20:14:47 +0530

    removed useless comments

--- app/src/main/java/com/rrawat/fileexplorer/ListFileActivity.java
+++ app/src/main/java/com/rrawat/fileexplorer/ListFileActivity.java
@@ -43,64 +43,10 @@ public class ListFileActivity extends ListActivity {
                 }
             }
         }
-        /*
-        List dirs = new ArrayList();
-        List files = new ArrayList();
-        FileFilter filterDirectoriesOnly = new FileFilter() {
-            public boolean accept(File file) {
-                return file.isDirectory();
-            }
-        };
-        FileFilter filterFilesOnly = new FileFilter() {
-            public boolean accept(File file) {
-                return !file.isDirectory();
-            }
-        };
-
-
-        File[] dirList = dir.listFiles(filterDirectoriesOnly);
-        File[] fileList = dir.listFiles(filterFilesOnly);
-        String[] dirArr=new String[dirList.length],fileArr=new String[fileList.length];
-        for (int i = 0; i < dirList.length; i++) {
-            File file = dirList[i];
-           // Log.d("***Rahul***",file.getName());
-            dirArr[i]=file.getName();
-        }
-        for (int i = 0; i < fileList.length; i++) {
-            File file = fileList[i];
-            fileArr[i]=file.getName();
-        }
-
-
-        if (dirArr != null) {
-            for (String file : dirArr) {
-                if (!file.startsWith(".")) {
-                    dirs.add(file);
-                }
-            }
-        }
-        if (fileArr != null) {
-            for (String file : fileArr) {
-                if (!file.startsWith(".")) {
-                    files.add(file);
-                }
-            }
-        }
-
-
-
-        Collections.sort(dirs);
-        Collections.sort(files);*/
         Collections.sort(values);
 
         // Put the data into the list
-
-//        this.setListAdapter(new ArrayAdapter<String>(this,R.layout.myfolder,R.id.Itemname,dirs));
-//        this.setListAdapter(new ArrayAdapter<String>(this,R.layout.myfiles,R.id.Itemname,files));
         setListAdapter(new FilesAndFoldersAdapter(this, values,path));
-        /*ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_2, android.R.id.text1, values);
-        setListAdapter(adapter);*/
 
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
